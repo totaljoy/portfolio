24e919e4d8222096a588b6f401b1b6ede9c1bb7e
Danielle Bruce <you@example.com>
2024-10-03 18:16:46 -0400

    Bug fixing again

--- src/components/HomeHero/HomeHero.jsx
+++ src/components/HomeHero/HomeHero.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
 import './HomeHero.scss'
+import React, { useEffect, useRef } from 'react'
 
 const HomeHero = () => {
 
     const marqueeText = ". ݁₊ ⊹ . ݁˖ . ݁Welcome to my website. ݁₊ ⊹ . ݁˖ . ݁ I love using my brain to solve creative problems. ݁₊ ⊹ . ݁˖ . ݁and getting to use my skills to work on projects I love";
+    const marqueeRef = useRef(null);
+
+    useEffect(() => {
+        const marquee = marqueeRef.current;
+        if (marquee) {
+          const marqueeWidth = marquee.offsetWidth;
+          const animationDuration = marqueeWidth / 50; // Adjust 50 to change speed
+          marquee.style.animationDuration = `${animationDuration}s`;
+        }
+      }, []);
+
     return (
     <main className='homepage'>
         <div className='homepage__marquee-container'>
-            <p className='homepage__marquee'>
-            {[...Array(4)].map((_, index) => (
-                <span key={index} className='homepage__marquee-item'>{marqueeText}</span>
-            ))}
-            </p>
+            <div className='homepage__marquee' ref={marqueeRef}>
+                <span className='homepage__marquee-content'>{marqueeText}</span>
+                <span className='homepage__marquee-content'>{marqueeText}</span>
+            </div>
         </div>
     </main>
     )
